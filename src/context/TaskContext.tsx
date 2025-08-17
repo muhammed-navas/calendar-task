a3ede6f76c259054b022caa47217938be8c42c1f
muhammed navas <muhammednavas6287@gmail.com>
2025-08-17 09:56:39 +0530

    update filter section 01

--- src/context/TaskContext.tsx
+++ src/context/TaskContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useReducer, useEffect, type ReactNode } from 'react';
+import { createContext, useContext, useReducer, type ReactNode } from 'react';
 import type { DragSelection, Task, TaskContextType, TaskCategory, FilterState } from '../types';
 
 type TaskAction =
